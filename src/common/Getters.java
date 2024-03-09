889933d157ce17b27d684fb8b2cf23f2e2fc1594
sarthak <sg622857@gmail.com>
2024-03-09 15:05:52 +0530

    Getter class added

---
+++ src/common/Getters.java
@@ -0,0 +1,21 @@
+package common;
+
+
+
+public class Getters {
+    public static void getSubjects(){
+
+    }
+
+    public static void getStudents(){
+
+    }
+
+    public static void getSubject(){
+
+    }
+
+    public static void getStudent(){
+        
+    }
+}
