2f5cc032628898affe84b1e4ba182e3b3d0b8bf4
sarthak <sg622857@gmail.com>
2024-03-10 01:37:58 +0530

    Database details hidden

--- src/common/SQL.java
+++ src/common/SQL.java
@@ -1,13 +1,19 @@
 package common;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class SQL {
     public static Connection makeConnection() throws Exception {
-        String url = "jdbc:mysql://localhost:3306/SIS";
-        String username = "root";
-        String password = "sarthak123";
+        Properties properties = new Properties();
+        try (InputStream input = SQL.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(input);
+        }
+        String url = properties.getProperty("db.url");
+        String username = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
         Connection c;
 
             Class.forName("com.mysql.cj.jdbc.Driver");
